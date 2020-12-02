86e07ecb435393ef16b407f354cc2d9146fa8d87
benoit17630 <benoit.menier@orange.fr>
2020-12-02 14:49:58 +0100

    ajout des fromages

---
+++ src/Controller/Admin/CheeseController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Admin\Cheese;
+use App\Form\Admin\CheeseType;
+use App\Repository\Admin\CheeseRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/cheese")
+ */
+class CheeseController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin_cheese_index", methods={"GET"})
+     */
+    public function index(CheeseRepository $cheeseRepository): Response
+    {
+        return $this->render('admin/cheese/index.html.twig', [
+            'cheeses' => $cheeseRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="admin_cheese_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $cheese = new Cheese();
+        $form = $this->createForm(CheeseType::class, $cheese);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($cheese);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_home');
+        }
+
+        return $this->render('admin/cheese/new.html.twig', [
+            'cheese' => $cheese,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_cheese_show", methods={"GET"})
+     */
+    public function show(Cheese $cheese): Response
+    {
+        return $this->render('admin/cheese/show.html.twig', [
+            'cheese' => $cheese,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="admin_cheese_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Cheese $cheese): Response
+    {
+        $form = $this->createForm(CheeseType::class, $cheese);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_home');
+        }
+
+        return $this->render('admin/cheese/edit.html.twig', [
+            'cheese' => $cheese,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_cheese_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Cheese $cheese): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$cheese->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($cheese);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_home');
+    }
+}
---
+++ src/Form/Admin/CheeseType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Form\Admin;
+
+use App\Entity\Admin\Cheese;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CheeseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Cheese::class,
+        ]);
+    }
+}
